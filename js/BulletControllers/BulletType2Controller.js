337376a43aa7bda6e8cae83924883c6d86818281
Tien <phamtien1709@gmail.com>
2017-08-04 13:27:54 +0700

    HomeWork Day3

--- js/BulletControllers/BulletType2Controller.js
+++ js/BulletControllers/BulletType2Controller.js
@@ -1,34 +1,37 @@
 class BulletType2Controller extends BulletController {
-  constructor(x, y, spriteSuffix, configs){
+  constructor(x, y, configs){
     super(x, y, "BulletType2.png", configs);
 
-    this.TURN_RATE = 5;
-    this.BULLET_SPEED = 200;
+    this.sprite.TURN_RATE = 5;
+    this.sprite.BULLET_SPEED = 200;
 
     this.sprite.update = this.update.bind(this);
   }
   update(){
     var targetAngle = Nakama.game.math.angleBetween(
-      this.sprite.position.x, this.sprite.position.y,
-      Nakama.enemies[0].sprite.x, Nakama.enemies[0].sprite.y
+      this.sprite.position.x,
+      this.sprite.position.y,
+      Nakama.enemies[0].sprite.x,
+      Nakama.enemies[0].sprite.y
     );
-    if (this.sprite.rotation !== targetAngle){
-      var delta = targetAngle - this.sprite.rotation;
+    if (this.sprite.rotation !== targetAngle + Math.PI/2){
+      var delta = targetAngle + Math.PI/2 - this.sprite.rotation;
       if(delta > Math.PI) delta -= Math.PI * 2;
       if(delta < -Math.PI) delta += Math.PI * 2;
 
       if(delta > 0){
-        this.sprite.angle += this.TURN_RATE;
+        this.sprite.rotation += Nakama.game.math.degToRad(this.sprite.TURN_RATE);
       }
       else {
-        this.sprite.angle -= this.TURN_RATE;
-      };
+        this.sprite.rotation -= Nakama.game.math.degToRad(this.sprite.TURN_RATE);
+      }
 
-      if (Math.abs(delta) < Nakama.game.math.degToRad(this.TURN_RATE)) {
-            this.sprite.rotation = targetAngle;
+      if (Math.abs(delta) < Nakama.game.math.degToRad(this.sprite.TURN_RATE)) {
+            this.sprite.rotation = targetAngle + Math.PI/2;
         }
-      this.sprite.body.velocity.x = Math.cos(this.sprite.rotation) * this.BULLET_SPEED;
-      this.sprite.body.velocity.y = Math.sin(this.sprite.rotation) * this.BULLET_SPEED;
+      this.sprite.body.velocity.x = Math.cos(this.sprite.rotation - Math.PI/2) * this.sprite.BULLET_SPEED;
+      this.sprite.body.velocity.y = Math.sin(this.sprite.rotation - Math.PI/2) * this.sprite.BULLET_SPEED;
     };
+    this.sprite.body.velocity.y = -this.sprite.BULLET_SPEED;
   }
 }
--- js/Shipcontrollers/ShipType2Controller.js
+++ js/Shipcontrollers/ShipType2Controller.js
@@ -14,13 +14,11 @@ class ShipType2Controller extends ShipController{
   fire(){
     new BulletType2Controller(
       this.sprite.position.x + this.configs.DIRECT_BULLET,
-      this.sprite.position.y,
-      {}
+      this.sprite.position.y
     );
     new BulletType2Controller(
       this.sprite.position.x - this.configs.DIRECT_BULLET,
-      this.sprite.position.y,
-      {}
+      this.sprite.position.y
     )
   }
 }

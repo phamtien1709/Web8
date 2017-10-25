eb864cbc85d821911b5e3327d89a462db71ee394
Phamtien <phamtien1709@gmail.com>
2017-10-25 20:25:39 +0700

    4/8/2017 (reverted from commit fa5cbab02da148fb0bb365ef1adfaed108aad9c4)

--- TechkidsHotgirl/server.js
+++ TechkidsHotgirl/server.js
@@ -6,12 +6,7 @@ const mongoose = require('mongoose');
 
 const config = require('./config.json');
 
-<<<<<<< HEAD
-const imageApi = require('./modules/api/images/imagesController');
-const usersApi = require('./modules/api/users/usersController');
-=======
 var imageApi = require('./modules/api/images/imagesController');
->>>>>>> parent of fa5cbab... 4/8/2017
 
 var app = express();
 
@@ -19,12 +14,6 @@ app.use(bodyParser.json({ extended : true}));
 app.use(bodyParser.urlencoded({ extended : true}));
 
 app.use('/api/image', imageApi);
-<<<<<<< HEAD
-app.use('/api/users', usersApi);
-
-app.use(express.static(__dirname + '/public'));
-=======
->>>>>>> parent of fa5cbab... 4/8/2017
 
 mongoose.connect(config.connectionString, (err) => {
   if (err) {

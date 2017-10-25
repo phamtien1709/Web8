73d5c93999f8eb098a0089332e94b542c8968e9d
Phamtien <phamtien1709@gmail.com>
2017-10-25 20:08:17 +0700

    ads

--- SaaS/app.js
+++
@@ -1,5 +0,0 @@
-const fileController = require('./fileController');
-const filename = 'F:/Techkids_Web_Gen8/SaaS/public/style.css';
-
-var a = fileController.readFile(filename);
-console.log(a);
--- js/BulletControllers/BulletType1Controller.js
+++
@@ -1,7 +0,0 @@
-class BulletType1Controller extends BulletController {
-  constructor(x, y, spriteSuffix, configs){
-    super(x, y, "BulletType1.png", configs);
-
-    this.sprite.body.velocity.y = -800;
-  }
-}
